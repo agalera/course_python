b384a5ba287f2130a1d42ec5e9bc1d28ac1718a9
Alberto Galera <galerajimenez@gmail.com>
2017-06-15 21:30:10 +0200

    resultado ejercicio

---
+++ examples/threading/clase/ejercicio.py
@@ -0,0 +1,42 @@
+import threading
+import queue
+import requests
+
+
+q = queue.Queue()
+sem = threading.BoundedSemaphore(2)
+lock = threading.Lock()
+result = 0
+
+r = requests.get('http://firecarrot.com:9000').json()['result']
+for url in r:
+    q.put(url)
+
+
+def worker():
+    global result
+    request = requests.Session()
+    while True:
+        try:
+            url = queue.get(timeout=10)
+        except queue.Empty:
+            break
+        with sem:
+            print("get", url)
+            r = request.get(url)
+        r = r.json()['result']
+        with lock:
+            result += r
+
+
+threads = []
+
+for x in range(4):
+    t = threading.Thread(target=worker)
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+print(result)  # 333283335000
